68fc39f8241ab460f6fa459b2df76c551c477b72
iktushar01 <iktushar2020@gmail.com>
2026-03-01 16:44:26 +0600

    not found middleware added

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { IndexRoute } from "./routes";
 import { globalErrorhandler } from "./middleware/globalErrorhandler";
-
+import { notFound } from "./middleware/notFound";
 const app = express();
 
 app.use(cors());
@@ -17,5 +17,5 @@ app.use("/api/v1", IndexRoute);
 
 
 app.use(globalErrorhandler);
-
+app.use(notFound);
 export default app;
