7bb89376da61b9b3622f64cc2a461c7f9bc6e944
iktushar01 <iktushar2020@gmail.com>
2026-03-12 09:24:38 +0600

    Setting Up CORS And Better Auth Trusted Origins

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,10 +7,18 @@ import { notFound } from "./middleware/notFound";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 import path from "node:path";
+import { envVars } from "../config/env";
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views",path.resolve(process.cwd(), `src/app/templates`) )
+
+app.use(cors({
+    origin : [envVars.FRONTEND_URL, envVars.BETTER_AUTH_URL, "http://localhost:3000", "http://localhost:5000"],
+    credentials : true,
+    methods : ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders : ["Content-Type", "Authorization"]
+}))
 
 app.use("/api/auth", toNodeHandler(auth))
 
