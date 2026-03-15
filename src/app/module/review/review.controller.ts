e8d598ba23381d7f368fb7faafe9b00a46c08b76
iktushar01 <iktushar2020@gmail.com>
2026-03-15 10:22:30 +0600

    review api write added

---
+++ src/app/module/review/review.controller.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express";
+import { catchAsync } from "../../shared/catchAsync";
+import { sendResponse } from "../../shared/sendResponse";
+import { ReviewService } from "./review.service";
+import { StatusCodes } from "http-status-codes";
+import { IRequestUser } from "../auth/auth.interface";
+
+const giveReview = catchAsync(async (req: Request, res: Response) => {
+    const payload = req.body;
+    const user = req.user as IRequestUser;
+    const result = await ReviewService.giveReview(user, payload);
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: 'Review created successfully',
+        data: result,
+    });
+});
+
+const getAllReviews = catchAsync(async (req: Request, res: Response) => {
+
+    const result = await ReviewService.getAllReviews();
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: 'Reviews retrieval successfully',
+        data: result
+    });
+});
+
+const myReviews = catchAsync(async (req: Request, res: Response) => {
+    const user = req.user as IRequestUser;
+    const result = await ReviewService.myReviews(user);
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: 'Reviews retrieval successfully',
+        data: result
+    });
+
+});
+
+const updateReview = catchAsync(async (req: Request, res: Response) => {
+    const user = req.user as IRequestUser;
+    const reviewId = req.params.id;
+    const payload = req.body;
+
+    const result = await ReviewService.updateReview(user, reviewId as string, payload);
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: 'Review updated successfully',
+        data: result
+    });
+}
+);
+
+const deleteReview = catchAsync(async (req: Request, res: Response) => {
+    const user = req.user as IRequestUser;
+    const reviewId = req.params.id;
+    const result = await ReviewService.deleteReview(user, reviewId as string);
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: 'Review deleted successfully',
+        data: result
+    });
+});
+
+
+export const ReviewController = {
+    giveReview,
+    getAllReviews,
+    myReviews,
+    updateReview,
+    deleteReview
+}
