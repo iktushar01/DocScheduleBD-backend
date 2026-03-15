8d11b6748770cb6b02d6863573374051c2b856c1
iktushar01 <iktushar2020@gmail.com>
2026-03-15 16:19:36 +0600

    update app.ts and server.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,32 +1,43 @@
-import express, { NextFunction, Request, Response } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import { IndexRoute } from "./routes";
-import { notFound } from "./middleware/notFound";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { toNodeHandler } from "better-auth/node";
-import { auth } from "./lib/auth";
-import path from "node:path";
-import { envVars } from "./config/env";
-import { globalErrorHandler } from "./middleware/globalErrorhandler";
-import qs from "qs";
-import { PaymentController } from "./module/payment/payment.controller";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
 import cron from "node-cron";
+import path from "path";
+import qs from "qs";
+import { envVars } from "./config/env";
+import { auth } from "./lib/auth";
+import { globalErrorHandler } from "./middleware/globalErrorhandler";
+import { notFound } from "./middleware/notFound";
 import { AppointmentService } from "./module/appointment/appointment.service";
-const app = express();
+import { PaymentController } from "./module/payment/payment.controller";
+import { IndexRoute } from "./routes";
 
-
+const app: Application = express();
 app.set("query parser", (str : string) => qs.parse(str));
+
 app.set("view engine", "ejs");
-app.set("views", path.resolve(process.cwd(), `src/app/templates`))
+app.set("views",path.resolve(process.cwd(), `src/app/templates`) )
 
 app.post("/webhook", express.raw({ type: "application/json" }), PaymentController.handleStripeWebhookEvent)
 
 app.use(cors({
-  origin: [envVars.FRONTEND_URL, envVars.BETTER_AUTH_URL, "http://localhost:3000", "http://localhost:5000"],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+    origin : [envVars.FRONTEND_URL, envVars.BETTER_AUTH_URL, "http://localhost:3000", "http://localhost:5000"],
+    credentials : true,
+    methods : ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders : ["Content-Type", "Authorization"]
 }))
+
+app.use("/api/auth", toNodeHandler(auth))
+
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
 
 cron.schedule("*/25 * * * *", async () => {
     try {
@@ -37,21 +48,18 @@ cron.schedule("*/25 * * * *", async () => {
     }
 })
 
-app.use("/api/auth", toNodeHandler(auth))
-
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("API is running 🚀");
-});
-
-
 app.use("/api/v1", IndexRoute);
 
+// Basic route
+app.get('/', async (req: Request, res: Response) => {
+    res.status(201).json({
+        success: true,
+        message: 'API is working',
+    })
+});
 
-app.use(globalErrorHandler);
-app.use(notFound);
+app.use(globalErrorHandler)
+app.use(notFound)
+
+
 export default app;
